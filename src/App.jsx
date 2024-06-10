43c8265e00ca18e5db3033b6dae3c3c453dab4b0
mfarhann10 <mfarhann1009@gmail.com>
2024-06-10 17:05:46 +0700

    create Todos and TodoItem component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import React, { useState } from 'react'
+import Todos from './components/Todos'
 
 function App() {
   const [todos, setTodos] = useState([
@@ -25,9 +26,7 @@ function App() {
   return (
     <div>
       <h1>My Todo List</h1>
-      {todos.map((todo) => {
-        return <p key={todo.id}>{todo.title}</p>
-      })}
+      <Todos todos = {todos}/>
     </div>
   )
 }
